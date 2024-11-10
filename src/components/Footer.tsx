d57bc2f29cb52c15864932903bc20f4e182a4611
thisismnjoo <mnjoo.real@gmail.com>
2024-11-10 22:05:23 +0900

    'Nov10'

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,28 @@
 export default function Footer() {
-  return <p>Footer</p>
+  return (
+    <div className="footer">
+      <div className="footer__name">
+        <span>
+          Department of SW&Tech, Korean Minjok Leadership Academy(KMLA)
+        </span>
+      </div>
+      <address className="footer__address">
+        800 Bonghwa-ro, Anheung-myeon, Hoengseong-gun, Gangwon-do
+      </address>
+      <div className="footer__contact">
+        <span>
+          <a
+            href="https://www.instagram.com/kmla_gwagibu/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @kmla_gwagibu
+          </a>
+        </span>
+      </div>
+      <div className="footer__copyright">
+        <small>ⓒ Gwagibu</small>
+      </div>
+    </div>
+  )
 }
--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,7 +1,28 @@
+import Nav from "../components/Nav.tsx"
+import Footer from "../components/Footer.tsx"
+
 export default function Login() {
   return (
     <div>
-      <h1>Login</h1>
+      <Nav />
+
+      <div className="container">
+        <div className="header">
+          <div className="text">Log in</div>
+          <div className="underline"></div>
+        </div>
+        <div className="inputs">
+          <div className="input">
+            <input type="email" placeholder="email" />
+          </div>
+          <div className="input">
+            <input type="password" placeholder="password" />
+          </div>
+        </div>
+        <div className="administrator">Administrator Only</div>
+      </div>
+
+      <Footer />
     </div>
   )
 }
